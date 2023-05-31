a6d842d5abac32fecdc94bc25ee6a43eb0b6ad35
bowenlin1101 <bowenblinblue@gmail.com>
2023-05-31 16:07:55 -0400

    initial commit

--- server/server.js
+++ server/server.js
@@ -5,79 +5,69 @@ import Reactor from "./reactor.js";
 const app = express();
 var reactor = new Reactor();
 
-app.use(express.static(path.join("..","build")))
+app.use(express.static(path.join("server","..","build")))
 app.use(express.static("public"))
 
 reactor.registerEvent("redOn")
 reactor.registerEvent("redOff")
 reactor.registerEvent("blueOn")
 reactor.registerEvent("blueOff")
+reactor.registerEvent("greenOn")
 reactor.registerEvent("greenOff")
-reactor.registerEvent("greenOff")
 
-app.post('/connect', (req,res) => {
-    
-    reactor.addEventListener("redOn",
-        ()=>{
-            res.send(`"redOn"`)
-        }
-    )
-
-    reactor.addEventListener("redOff",
-        ()=>{
-            res.send(`"redOff"`)
-        }
-    )
-
-    reactor.addEventListener("blueOn",
-        ()=>{
-            res.send(`"blueOn"`)
-        }
-    )
-
-    reactor.addEventListener("blueOff",
-        ()=>{
-            res.send(`"blueOff"`)
-        }
-    )
-
-    reactor.addEventListener("greenOn",
-        ()=>{
-            res.send(`"greenOn"`)
-        }
-    )
-
-    reactor.addEventListener("greenOff",
-        ()=>{
-            res.send(`"greenOff"`)
-        }
-    )
+app.get('/connect', (req,res) => {
+    res.write("Connected")
+    console.log("ESP32 is connected")
+    reactor.addEventListener("redOn",()=>{
+        res.write(`redOn`)
+    })
+    reactor.addEventListener("redOff",()=>{
+        res.write(`redOff`)
+    })
+    reactor.addEventListener("blueOn",()=>{
+        res.write(`blueOn`)
+    })
+    reactor.addEventListener("blueOff",()=>{
+        res.write(`blueOff`)
+    })
+    reactor.addEventListener("greenOn",()=>{
+        res.write(`greenOn`)
+    })
+    reactor.addEventListener("greenOff",()=>{
+        res.write(`greenOff`)
+    })
 })
 
 app.post("/redOn",(req,res) => {
     reactor.dispatchEvent("redOn")
-    console.log("ON")
+    res.send("on")
 })
 
 app.post("/redOff",(req,res) => {
-    reactor.dispatchEvent(redOff)
-
+    reactor.dispatchEvent("redOff")
+    res.send("off")
 })
 
 app.post("/blueOn",(req,res) => {
-    reactor.dispatchEvent(blueOn)
+    reactor.dispatchEvent("blueOn")
+    res.send("on")
 })
 
 app.post("/blueOff",(req,res) => {
-    reactor.dispatchEvent(blueOff)
+    reactor.dispatchEvent("blueOff")
+    res.send("off")
 })
 
 app.post("/greenOn",(req,res) => {
-    reactor.dispatchEvent(greenOn)
+    reactor.dispatchEvent("greenOn")
+    res.send("on")
 })
 
 app.post("/greenOff",(req,res) => {
-    reactor.dispatchEvent(greenOff)
+    reactor.dispatchEvent("greenOff")
+    res.send("off")
 })
 
-app.listen(8000);
+app.listen(80,()=>{
+        console.log("server running on port 80");
+});
--- src/App.js
+++ src/App.js
@@ -1,106 +1,90 @@
 import './styles.css';
-import React, { useEffect, useState } from "react";
+import React, {useState } from "react";
 
-
-function App (props) {
-  const [red, setRed] = useState(false);
-  const [blue, setBlue] = useState(false);
-  const [green, setGreen] = useState(false);
-
-  return (
-      <>
-          <h1>ESP32 LED Control Panel</h1>
-          <div id="main-container">
-              <Red red={red} setRed={setRed}/>
-              <Blue blue={blue} setBlue={setBlue}/>
-              <Green green={green} setGreen={setGreen}/>
-          </div>
-      </>
-  )
+function App () {
+    const [red, setRed] = useState(false);
+    const [blue, setBlue] = useState(false);
+    const [green, setGreen] = useState(false);
+    return (<>
+        <h1>ESP32 LED Control Panel</h1>
+        <div id="main-container">
+            <Red red={red} setRed={setRed}/>
+            <Blue blue={blue} setBlue={setBlue}/>
+            <Green green={green} setGreen={setGreen}/>
+        </div>
+    </>)
 }
 
 function Red (props){
-  const state = props.red ? "Current state: On" : "Current state: Off"
-  const text = props.red ? "Turn Off" : "Turn On"
-  const styles = props.red ? "on" : "off"
-  function onClick(){
-      const extension = props.red ? "/redOff" : "redOn"
-      var response = fetch(`${extension}`, {
-          method: "POST"
-      });
-      
-      response.then((result) => {
-          if (result === "on"){
-              props.setRed(true)
-          } else {
-              props.setRed(false)
-          }
-      })
-      .catch((error) => {
+    const state = props.red ? "Current state: On" : "Current state: Off"
+    const text = props.red ? "Turn Off" : "Turn On"
+    const styles = props.red ? "on" : "off"
+    function onClick(){
+        const extension = props.red ? "redOff" : "redOn"
+        var response = fetch(`${extension}`, {
+            method: "POST"
+        });
+        response.then(() => {
+            props.setRed(!props.red)
+        })
+        .catch((error) => {
           console.log(error)
-      })
-  }
+        })
+    }
 
-  return <>
-      <h2>{state}</h2>
-      <button className={`red-button ${styles}`} onClick={onClick}>{text}</button>
-  </>
+    return <>
+        <h2>{state}</h2>
+        <button className={`red-button ${styles}`} onClick={onClick}>{text}</button>
+    </>
 }
 
 function Blue (props) {
-  const state = props.blue ? "Current state: On" : "Current state: Off"
-  const text = props.blue ? "Turn Off" : "Turn On"
-  const styles = props.blue ? "on" : "off"
-  function onClick(){
-      const extension = props.blue ? "/blueOff" : "blueOn"
-      var response = fetch(`${extension}`, {
-          method: "POST"
-      });
-      
-      response.then((result) => {
-          if (result === "on"){
-              props.setBlue(true)
-          } else {
-              props.setBlue(false)
-          }
-      })
-      .catch((error) => {
-          console.log(error)
-      })
-  }
+    const state = props.blue ? "Current state: On" : "Current state: Off"
+    const text = props.blue ? "Turn Off" : "Turn On"
+    const styles = props.blue ? "on" : "off"
+    function onClick(){
+        const extension = props.blue ? "/blueOff" : "blueOn"
+        var response = fetch(`${extension}`, {
+            method: "POST"
+        });
 
-  return <>
-      <h2>{state}</h2>
-      <button className={`blue-button ${styles}`} onClick={onClick}>{text}</button>
-  </>
+        response.then(() => {
+            props.setBlue(!props.blue)
+        })
+        .catch((error) => {
+            console.log(error)
+        })
+    }
+
+    return <>
+        <h2>{state}</h2>
+        <button className={`blue-button ${styles}`} onClick={onClick}>{text}</button>
+    </>
 }
-
+  
 function Green (props) {
-  const state = props.green ? "Current state: On" : "Current state: Off"
-  const text = props.green ? "Turn Off" : "Turn On"
-  const styles = props.green ? "on" : "off"
-  function onClick(){
-      const extension = props.green ? "/greenOff" : "greenOn"
-      var response = fetch(`${extension}`, {
-          method: "POST"
-      });
-      
-      response.then((result) => {
-          if (result === "on"){
-              props.setGreen(true)
-          } else {
-              props.setGreen(false)
-          }
-      })
-      .catch((error) => {
-          console.log(error)
-      })
-  }
+    const state = props.green ? "Current state: On" : "Current state: Off"
+    const text = props.green ? "Turn Off" : "Turn On"
+    const styles = props.green ? "on" : "off"
+    function onClick(){
+        const extension = props.green ? "/greenOff" : "greenOn"
+        var response = fetch(`${extension}`, {
+            method: "POST"
+        });
 
-  return <>
-      <h2>{state}</h2>
-      <button className={`green-button ${styles}`} onClick={onClick}>{text}</button>
-  </>
+        response.then(() => {
+            props.setGreen(!props.green)
+        })
+        .catch((error) => {
+            console.log(error)
+        })
+    }
+
+    return <>
+        <h2>{state}</h2>
+        <button className={`green-button ${styles}`} onClick={onClick}>{text}</button>
+    </>
 }
-
+  
 export default App;
+  
