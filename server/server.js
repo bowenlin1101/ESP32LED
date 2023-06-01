789f9aa3fcc50ee5b6cf418cc9d909233038eb26
bowenlin1101 <bowenblinblue@gmail.com>
2023-06-01 14:05:48 -0400

    added webClient endpoint, clients id array, states, and client notification

--- server/server.js
+++ server/server.js
@@ -1,12 +1,11 @@
 import express from "express";
+import bodyParser from "body-parser";
 import * as path from 'path';
+import cors from 'cors';
 import Reactor from "./reactor.js";
 
 const app = express();
 var reactor = new Reactor();
-
-app.use(express.static(path.join("server","..","build")))
-app.use(express.static("public"))
 
 reactor.registerEvent("redOn")
 reactor.registerEvent("redOff")
@@ -15,9 +14,54 @@ reactor.registerEvent("blueOff")
 reactor.registerEvent("greenOn")
 reactor.registerEvent("greenOff")
 
+app.use(express.static(path.join("server","..","build")))
+app.use(express.static("public"))
+app.use(cors())
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({extended: false}));
+
+const PORT = 8000;
+
+let clients = [];
+let states = {red: false, blue: false, green: false}
+function eventsHandler(request, response, next) {
+    console.log("Webclient Connected")
+    const headers = {
+      'Content-Type': 'text/event-stream',
+      'Connection': 'keep-alive',
+      'Cache-Control': 'no-cache'
+    };
+    response.writeHead(200, headers);
+  
+    const data = `data: ${JSON.stringify(states)}\n\n`;
+  
+    response.write(data);
+  
+    const clientId = Date.now();
+  
+    const newClient = {
+      id: clientId,
+      response
+    };
+  
+    clients.push(newClient);
+  
+    request.on('close', () => {
+      console.log(`${clientId} Connection closed`);
+      clients = clients.filter(client => client.id !== clientId);
+    });
+}
+
+app.get('/webClients', eventsHandler);
+
+function sendEventsToAll() {
+    clients.forEach(client => client.response.write(`data: ${JSON.stringify(states)}\n\n`))
+}
+
 app.get('/connect', (req,res) => {
     res.write("Connected")
     console.log("ESP32 is connected")
+
     reactor.addEventListener("redOn",()=>{
         res.write(`redOn`)
     })
@@ -38,36 +82,48 @@ app.get('/connect', (req,res) => {
     })
 })
 
-app.post("/redOn",(req,res) => {
+app.post("/redOn", async (req,res) => {
+    states.red = true;
+    res.json(states)
     reactor.dispatchEvent("redOn")
-    res.send("on")
+    return sendEventsToAll();
 })
 
-app.post("/redOff",(req,res) => {
+app.post("/redOff", async (req,res) => {
+    states.red = false;
     reactor.dispatchEvent("redOff")
-    res.send("off")
+    res.json(states)
+    return sendEventsToAll()
 })
 
-app.post("/blueOn",(req,res) => {
+app.post("/blueOn",async (req,res) => {
+    states.blue = true;
     reactor.dispatchEvent("blueOn")
-    res.send("on")
+    res.json(states)
+    return sendEventsToAll()
 })
 
-app.post("/blueOff",(req,res) => {
+app.post("/blueOff",async (req,res) => {
+    states.blue = false;
     reactor.dispatchEvent("blueOff")
-    res.send("off")
+    res.json(states)
+    return sendEventsToAll()
 })
 
-app.post("/greenOn",(req,res) => {
+app.post("/greenOn",async (req,res) => {
+    states.green = true;
     reactor.dispatchEvent("greenOn")
-    res.send("on")
+    res.json(states)
+    return sendEventsToAll()
 })
 
-app.post("/greenOff",(req,res) => {
+app.post("/greenOff",async (req,res) => {
+    states.green = false;
     reactor.dispatchEvent("greenOff")
-    res.send("off")
+    res.json(states)
+    return sendEventsToAll()
 })
 
-app.listen(80,()=>{
-        console.log("server running on port 80");
+app.listen(PORT,()=>{
+        console.log(`server running on port ${PORT}`);
 });
