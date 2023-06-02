5386c118a56306bea1ecdd19331e739fde352032
bowenlin1101 <bowenblinblue@gmail.com>
2023-06-02 13:59:35 -0400

    added button debounce

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import './styles.css';
-import React, {useEffect, useState } from "react";
+import React, {useCallback, useEffect, useState } from "react";
+
 
 function App () {
     const [red, setRed] = useState(false);
@@ -35,16 +36,20 @@ function Red (props){
     const state = props.red ? "Current state: On" : "Current state: Off"
     const text = props.red ? "Turn Off" : "Turn On"
     const styles = props.red ? "on" : "off"
-    function onClick(){
-        const extension = props.red ? "redOff" : "redOn"
+    function onClick(red){
+        const extension = red ? "redOff" : "redOn"
         fetch(`${extension}`, {
             method: "POST"
         });
     }
 
+    const debouncedClick = useCallback(
+        debounce_leading((red) => onClick(red))
+    ,[])
+
     return <>
         <h2>{state}</h2>
-        <button className={`red-button ${styles}`} onClick={onClick}>{text}</button>
+        <button className={`red-button ${styles}`} onClick={() => debouncedClick(props.red)}>{text}</button>
     </>
 }
 
@@ -52,16 +57,20 @@ function Blue (props) {
     const state = props.blue ? "Current state: On" : "Current state: Off"
     const text = props.blue ? "Turn Off" : "Turn On"
     const styles = props.blue ? "on" : "off"
-    function onClick(){
-        const extension = props.blue ? "/blueOff" : "blueOn"
+    function onClick(blue){
+        const extension = blue ? "blueOff" : "blueOn"
         fetch(`${extension}`, {
             method: "POST"
         });
     }
 
+    const debouncedClick = useCallback(
+        debounce_leading((blue) => onClick(blue))
+    ,[])
+
     return <>
         <h2>{state}</h2>
-        <button className={`blue-button ${styles}`} onClick={onClick}>{text}</button>
+        <button className={`blue-button ${styles}`} onClick={() => debouncedClick(props.blue)}>{text}</button>
     </>
 }
   
@@ -69,18 +78,35 @@ function Green (props) {
     const state = props.green ? "Current state: On" : "Current state: Off"
     const text = props.green ? "Turn Off" : "Turn On"
     const styles = props.green ? "on" : "off"
-    function onClick(){
-        const extension = props.green ? "/greenOff" : "greenOn"
+    function onClick(green){
+        const extension = green ? "greenOff" : "greenOn"
         fetch(`${extension}`, {
             method: "POST"
         });
     }
 
+    const debouncedClick = useCallback(
+        debounce_leading((green) => onClick(green))
+    ,[])
+
     return <>
         <h2>{state}</h2>
-        <button className={`green-button ${styles}`} onClick={onClick}>{text}</button>
+        <button className={`green-button ${styles}`} onClick={() => debouncedClick(props.green)}>{text}</button>
     </>
 }
   
+function debounce_leading(func, timeout = 300){
+    let timer;
+    return (...args) => {
+      if (!timer) {
+        func.apply(this, args);
+      }
+      clearTimeout(timer);
+      timer = setTimeout(() => {
+        timer = undefined;
+      }, timeout);
+    };
+}
+
 export default App;
   
